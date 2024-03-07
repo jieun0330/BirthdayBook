8b36ba52a0da6778083f177850e22019dcdf96dc
jieun <jieun_0330@naver.com>
2024-03-07 14:10:56 +0900

    init: initial commit

---
+++ BirthdayBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayBook
+//
+//  Created by 박지은 on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
