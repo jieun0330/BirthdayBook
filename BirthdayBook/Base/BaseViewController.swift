fb73a99820e34eb205c33a5120439f2b069f3762
jieun <jieun_0330@naver.com>
2024-03-07 15:49:54 +0900

    chore: Then 라이브러리 추가

---
+++ BirthdayBook/Base/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  BirthdayBook
+//
+//  Created by 박지은 on 3/7/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BirthdayBook/Calendar/CalendarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarViewController.swift
+//  BirthdayBook
+//
+//  Created by 박지은 on 3/7/24.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
