6a621ac30899865a7a97d4ac836fbe004bfecbaf
jieun <jieun_0330@naver.com>
2024-03-09 19:48:10 +0900

    feat: gitignore 설정

---
+++ BirthdayBook/Manager/APIManager.swift
@@ -0,0 +1,16 @@
+//
+//  APIManager.swift
+//  BirthdayBook
+//
+//  Created by 박지은 on 3/9/24.
+//
+
+import Foundation
+
+class APIManager {
+    
+    func callRequest() {
+        let url = "https://www.nl.go.kr/seoji/SearchApi.do?cert_key=[발급된키값]&result_style=json&page_no=1&page_size=10&start_publish_date=20220509&end_publish_date=20220509"
+    }
+    
+}
