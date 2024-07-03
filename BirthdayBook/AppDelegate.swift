32ca13fc042e3e5140f275cd45edea09ef438033
jieun <jieun_0330@naver.com>
2024-07-03 18:27:56 +0900

    style: Splash 화면 구현

--- BirthdayBook/AppDelegate.swift
+++ BirthdayBook/AppDelegate.swift
@@ -12,6 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 스플레시 딜레이
+        Thread.sleep(forTimeInterval: 3.0)
+        
         // Xcode Project - Firebase 연결
 //        FirebaseApp.configure()
         return true
